909c1105549f725fdb3e5337611f02ec7f36f94b
Janet Villarreal <jvillarr20@gmail.com>
2023-10-07 17:18:01 -0700

    fixed warnings

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "66edc77486948980"; }
+/******/ 		__webpack_require__.h = function() { return "ab2dca9097135281"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
